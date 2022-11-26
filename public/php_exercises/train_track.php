7596b141e719d31e088fbee2aaaba563b3116dbc
Ramkumar010 <snehabiotech2004@gmail.com>
2022-11-26 06:07:59 +0000

    if condition modified

--- public/php_exercises/train_track.php
+++ public/php_exercises/train_track.php
@@ -6,7 +6,7 @@ function train_track($pairs){
     //echo $mod_string;
     $x=substr_count ($mod_string,"F");
     $y=substr_count ($mod_string,"M");
-    if ($x==$y){
+    if (($x==$y) and (count($mod_pairs)>1)){
         return "LOOP<br>";
     } else {
         return "NO LOOP<br>";
@@ -18,4 +18,5 @@ echo train_track("FM FF MF MM");
 echo train_track("MM FF");
 echo train_track("MF MF MF MF FF");
 echo train_track("mm ff ff MM");
+echo train_track("mf");
 ?>
