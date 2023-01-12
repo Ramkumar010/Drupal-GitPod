f101a06ba18e22bdb320e32c4842b79c8be42324
Ramkumar010 <snehabiotech2004@gmail.com>
2023-01-12 08:01:47 +0000

    problem solving

--- public/problem_set-4/maximum_product.py
+++ public/problem_set-4/maximum_product.py
@@ -1,12 +1,15 @@
 def maximum_product(array):
-    max1=array[0]
-    max2=array[1]
-    for i in array:
-        if i>max1:
-            max2=max1
-            max1=i
-        elif ((i>max2) and (i!=max1)):
-            max2=i 
-    product=max1*max2
-    print(product)
-maximum_product([10,100,20,40,102,22])    
+    if len(array)>1:
+        max1=array[0]
+        max2=array[1]
+        for i in array:
+            if i>max1:
+                max2=max1
+                max1=i
+            elif ((i>max2) and (i!=max1)):
+                max2=i 
+        product=max1*max2
+        print(product)
+    else:
+        print("number of list elements should be more than one.")
+maximum_product([10,2])   
